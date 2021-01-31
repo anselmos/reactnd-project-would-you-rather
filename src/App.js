6681f6a0114ea724f06f02539ae062f9704f34ba
Anselmos <anselmos@users.noreply.github.com>
2021-01-31 17:41:21 +0100

    Adds redux to package.json. Adds initial version for reading users data from DATA store.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,41 @@ import './spacer.css';
 import NavigationHeader from "./NavigationHeader";
 import DataBody from "./DataBody";
 import NewQuestion from "./NewQuestion";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {_getUsers} from "./_DATA";
+const RECEIVE_DATA = "RECEIVE_DATA";
+
+function receiveDataAction (users) {
+  return {
+    type: RECEIVE_DATA,
+    users,
+  }
+}
+
+function users(state= [], action){
+    switch(action.type){
+        case RECEIVE_DATA:
+            return action.users
+        default:
+            return state
+    }
+}
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type)
+    console.log('The action: ', action)
+    const result = next(action)
+    console.log('The new state: ', store.getState())
+  console.groupEnd()
+  return result
+}
+
+async function getOrUpdateUserData(){
+    const users = await _getUsers();
+    this.store.dispatch(receiveDataAction(users))
+}
 class App extends React.Component{
+
     constructor(props) {
         super(props);
         this.state = {
@@ -22,6 +56,9 @@ class App extends React.Component{
                 questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
           }
         }
+       this.store = createStore(combineReducers({
+            users,
+        }), applyMiddleware(logger))
     }
     handleLogin() {
       this.setState({user: {
@@ -37,7 +74,11 @@ class App extends React.Component{
             questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
       }})
     }
-  handleLogout() {
+    componentDidMount() {
+        getOrUpdateUserData.call(this);
+    }
+
+    handleLogout() {
         this.setState({user: null});
   }
   render(){
