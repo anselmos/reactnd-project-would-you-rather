c9a2b271c001c852cba1a37edef59d43a1a806db
Anselmos <anselmos@users.noreply.github.com>
2021-02-28 18:44:46 +0100

    Adds avatarUrl for login-button.

--- src/api/_DATA.js
+++ src/api/_DATA.js
@@ -1,8 +1,11 @@
+import sarah from "../style/sarah.jpg"
+import john from "../style/john.jpg"
+import tyler from "../style/tyler.jpg"
 let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: '',
+    avatarURL: sarah,
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +17,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: '',
+    avatarURL: tyler,
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +27,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: '',
+    avatarURL: john,
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
